7d48328835ca312020881b4d2785485085e2f538
Smit4545 <smitthakkar9956@gmail.com>
2025-02-19 13:41:27 +0530

    sdf

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -6,9 +6,9 @@ export const useSocket = () => {
 
   useEffect(() => {
     const serverUrl =
-      process.env.NODE_ENV === "development"
+      process.env.NODE_ENV !== "development"
         ? "http://localhost:3000" // Local development URL
-        : "https://e6f3-2401-4900-1c80-a3e0-b034-f274-1ad2-732f.ngrok-free.app"; // ngrok URL
+        : "https://socialsssss.vercel.app"; // ngrok URL
 
     const socketInstance = io(serverUrl, {
       path: "/api/socket.io",
