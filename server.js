f8766e4e9a75dc4623064256455cbd70760c0c13
Smit4545 <smitthakkar9956@gmail.com>
2025-02-19 15:28:15 +0530

    ddd

--- server.js
+++ server.js
@@ -105,10 +105,9 @@ const next = require("next");
 const { Server } = require("socket.io");
 
 const dev = process.env.NODE_ENV !== "production";
-const hostname = "localhost";
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-const app = next({ dev, hostname, port });
+const app = next({ dev });
 const handler = app.getRequestHandler();
 
 app.prepare().then(() => {
