abd91f100ba6bb7174961a30191162ac944b1d82
Smit4545 <smitthakkar9956@gmail.com>
2025-02-19 18:54:44 +0530

    th

--- src/app/(auth)/Login/page.jsx
+++ src/app/(auth)/Login/page.jsx
@@ -27,7 +27,7 @@ const LogIn = () => {
         document.cookie = `userId=${data.id}; path=/; max-age=3600`;
         localStorage.setItem("userId", data.id);
         localStorage.setItem("username", data.name);
-        router.push(`${process.env.URL}/`);
+        router.push(`${process.env.URL}`);
       } else {
         setError(data.error || "Failed to log in");
       }
