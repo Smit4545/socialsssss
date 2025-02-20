0c9fa533eeb9545f56f6c97a6ad69d70dedbf38f
Smit4545 <smitthakkar9956@gmail.com>
2025-02-20 13:25:51 +0530

    efefefef

--- src/middleware.js
+++ src/middleware.js
@@ -1,31 +1,32 @@
-import { NextResponse } from 'next/server'
+import { NextResponse } from "next/server";
 
 export function middleware(request) {
-  const path = request.nextUrl.pathname
+  const path = request.nextUrl.pathname;
 
-  const isPublicPath = path === '/Login' || path === '/signin'
+  const isPublicPath = path === "/Login" || path === "/signin";
 
-  const token = request.cookies.get('token')?.value || ''
+  const token = request.cookies.get("token")?.value || "";
 
   // Redirect to home if the user is logged in and tries to access login/signin
   if (isPublicPath && token) {
-    return NextResponse.redirect(new URL('/', request.nextUrl))
+    return NextResponse.redirect(new URL("/", request.nextUrl));
   }
 
   // Redirect to login if the user is not logged in and tries to access protected routes
   if (!isPublicPath && !token) {
-    return NextResponse.redirect(new URL('/Login', request.nextUrl))
+    return NextResponse.redirect(new URL("/Login", request.nextUrl));
   }
-    
-  return NextResponse.next() // Proceed to the requested page if the conditions are met
+
+  return NextResponse.next(); // Proceed to the requested page if the conditions are met
 }
 
 export const config = {
   matcher: [
-    '/', // Home page
-    '/profile', // Profile page
-    '/friends', // Friends page
-    '/Login', // Login page
-    '/signin', // Signin page
-  ]
-}
+    "/", // Home page
+    "/profile", // Profile page
+    "/friends", // Friends page
+    "/location",
+    "/Login", // Login page
+    "/signin", // Signin page
+  ],
+};
