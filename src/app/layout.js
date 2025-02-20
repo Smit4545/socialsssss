d43f3b0429deb85196aff07a2f97f6587bd7c811
Smit4545 <smitthakkar9956@gmail.com>
2025-02-20 16:28:02 +0530

    efewfre

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,9 +26,24 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+
+        {/* Apple Touch Icon for iOS */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+
+        {/* Android home screen icon */}
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+
+        <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
+
+        {/* Manifest for PWA */}
+      </head>
       <body>
-      <Toaster position="top-right" reverseOrder={false} />
-        {pathname === "/" ? children : <Layout>{children}</Layout>}</body>
+        <Toaster position="top-right" reverseOrder={false} />
+        {pathname === "/" ? children : <Layout>{children}</Layout>}
+      </body>
     </html>
   );
 }
